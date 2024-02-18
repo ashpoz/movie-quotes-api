79948116b5addc265fe678f40149238dfcea8cde
hellomaryjo <majorx3music@gmail.com>
2024-02-18 15:44:33 -0600

    Update port?

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const v1WorkoutRouter = require("./v1/routes/quotesRouter.js");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use("/api/v1/quotes", v1WorkoutRouter);
 
